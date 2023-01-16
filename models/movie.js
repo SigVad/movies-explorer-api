8b2b92bb9a12100efebe21a5846b25bbdb93fb1d
SigVad <97845386+SigVad@users.noreply.github.com>
2023-01-16 22:03:39 +0300

    feat/ Схемы пользователей и фильмов

---
+++ models/movie.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const isURL = require('validator/lib/isURL');
+
+// схема Cохранённые фильмы
+const movieSchema = new mongoose.Schema({
+  country: { // страна создания фильма
+    type: String,
+    required: true,
+  },
+  director: { // режиссёр фильма
+    type: String,
+    required: true,
+  },
+  duration: { // длительность фильма
+    type: Number,
+    required: true,
+  },
+  year: { // год выпуска фильма
+    type: String,
+    required: true,
+  },
+  description: { // описание фильма
+    type: String,
+    required: true,
+  },
+  image: { // ссылка на постер к фильму
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => isURL(link),
+      message: 'Некорректная ссылка',
+    },
+  },
+  trailerLink: { // ссылка на трейлер фильма
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => isURL(link),
+      message: 'Некорректная ссылка',
+    },
+  },
+  thumbnail: { // миниатюрное изображение постера к фильму
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => isURL(link),
+      message: 'Некорректная ссылка',
+    },
+  },
+  owner: { // _id пользователя, который сохранил фильм
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  movieId: { // id фильма, который содержится в ответе сервиса MoviesExplorer
+    type: Number,
+    required: true,
+  },
+  nameRU: { // название фильма на русском языке
+    type: String,
+    required: true,
+  },
+  nameEN: { // название фильма на английском языке
+    type: String,
+    required: true,
+  },
+});
+
+// модель
+module.exports = mongoose.model('card', movieSchema);
