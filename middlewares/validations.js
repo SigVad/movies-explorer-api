e398188d426e453a853667f43cce9bdbfdd1be62
SigVad <97845386+SigVad@users.noreply.github.com>
2023-01-16 23:24:28 +0300

    Валидация users, movies

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -1,6 +1,7 @@
 const { celebrate, Joi } = require('celebrate');
+const { httpRegex } = require('../utils/constants');
 
-// '/signup'
+// post '/signup'
 const createUserValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email({ tlds: { allow: false } }),
@@ -9,7 +10,7 @@ const createUserValidator = celebrate({
   }),
 });
 
-// '/signin'
+// post '/signin'
 const loginValidator = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email({ tlds: { allow: false } }),
@@ -17,6 +18,38 @@ const loginValidator = celebrate({
   }),
 });
 
+// post '/movies'
+const createMovieValidator = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().regex(httpRegex),
+    trailer: Joi.string().required().regex(httpRegex),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    thumbnail: Joi.string().required().regex(httpRegex),
+    movieId: Joi.number().required(),
+  }),
+});
+
+// delete '/movies/:movieId'
+const deleteMovieValidator = celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().required().hex().length(24),
+  }),
+});
+
+// patch '/users/me'
+const updateUserValidator = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email({ tlds: { allow: false } }),
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
 module.exports = {
-  createUserValidator, loginValidator,
+  createUserValidator, loginValidator, createMovieValidator, deleteMovieValidator, updateUserValidator,
 };
