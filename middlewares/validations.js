53aa8938602746610a388a4232aa43b87fce1ff6
SigVad <97845386+SigVad@users.noreply.github.com>
2023-01-16 22:33:38 +0300

    валидация signup, signin

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -1,5 +1,4 @@
 const { celebrate, Joi } = require('celebrate');
-const { httpRegex } = require('../utils/constants');
 
 // '/signup'
 const createUserValidator = celebrate({
@@ -18,35 +17,6 @@ const loginValidator = celebrate({
   }),
 });
 
-const updateUserValidator = celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().email({ tlds: { allow: false } }),
-  }),
-});
-
-const createMovieValidator = celebrate({
-  body: Joi.object().keys({
-    country: Joi.string().required(),
-    director: Joi.string().required(),
-    duration: Joi.number().required(),
-    year: Joi.string().required(),
-    description: Joi.string().required(),
-    image: Joi.string().required().regex(httpRegex),
-    trailerLink: Joi.string().required().regex(httpRegex),
-    thumbnail: Joi.string().required().regex(httpRegex),
-    movieId: Joi.number().required(),
-    nameRU: Joi.string().required(),
-    nameEN: Joi.string().required(),
-  }),
-});
-
-const deleteMovieValidator = celebrate({
-  params: Joi.object().keys({
-    movieId: Joi.string().required().hex().length(24),
-  }),
-});
-
 module.exports = {
-  createUserValidator, loginValidator, updateUserValidator, createMovieValidator, deleteMovieValidator,
+  createUserValidator, loginValidator,
 };
