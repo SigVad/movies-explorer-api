7377304c71f1f6aa0d981446ee3ba0969f3fb1f1
SigVad <97845386+SigVad@users.noreply.github.com>
2023-01-16 22:40:05 +0300

    Ошибка по-умолчанию

---
+++ middlewares/defaultError.js
@@ -0,0 +1,10 @@
+const defaultError = (err, req, res, next) => {
+  const {
+    statusCode = 500,
+    message = 'На сервере произошла ошибка',
+  } = err;
+  res.status(statusCode).send({ message });
+  next();
+};
+
+module.exports = defaultError;
